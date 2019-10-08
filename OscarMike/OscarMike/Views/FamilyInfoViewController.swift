a9de5a68cf06020354f24d213c443713c23dd773
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-10-08 14:29:45 -0400

    begin redesign

---
+++ OscarMike/OscarMike/Views/FamilyInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FamilyInfoViewController.swift
+//  OscarMike
+//
+//  Created by Thomas Cowern New on 10/8/19.
+//  Copyright © 2019 Thomas Cowern New. All rights reserved.
+//
+
+import UIKit
+
+class FamilyInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
